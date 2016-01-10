adcf81350c4d706672fb6b705b389b52c2a8091c
mrmaninew <manikanta@live.in>
2016-01-11 00:29:45 +0530

    made few changes

--- www/js/services.js
+++ www/js/services.js
@@ -3,9 +3,17 @@ angular.module('starter.services', [])
     	'Client_id': 'ac0dd3408c1031006907010c2cc6ef6d',
         'Client_secret': '1yihwfk2xbl686v45s8a',
         'grant_type': ['password','access'],
-        'prod_url': 'https://volteollcdemo1.service-now.com/oauth_token.do'
+     /* this url, might be changed from setting panel, so we have to set this url and to _url
+        and add "oauth_token.do" as additive so wer can access tokens and same with access to 
+        tables  */
+        'prod_url': 'https://volteollcdemo1.service-now.com/oauth_token.do',
+        'username':''
     })
     .factory('MainService', function() {
+
+    })
+    .factory('InstanceUrl',function(){
+        var url_oauth = ""+'oauth_token.do';
 
     })
     .factory('TokenService', function() {
