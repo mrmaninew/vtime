91240c8f80613dc6181e462535d5524fa6969ad8
mrmaninew <manikanta@live.in>
2016-01-13 09:47:20 +0530

    made some changes

--- www/js/services.js
+++ www/js/services.js
@@ -3,9 +3,9 @@ angular.module('starter.services', [])
         'Client_id': 'ac0dd3408c1031006907010c2cc6ef6d',
         'Client_secret': '1yihwfk2xbl686v45s8a',
         'grant_type': ['password', 'access'],
- /* this url, might be changed from setting panel, so we have to set this url and to _url
-    and add "oauth_token.do" as additive so wer can access tokens and same with access to 
-    tables  */
+        /* this url, might be changed from setting panel, so we have to set this url and to _url
+           and add "oauth_token.do" as additive so wer can access tokens and same with access to 
+           tables  */
         'url': 'https://volteollcdemo1.service-now.com/', // Servicenow Instance URL
         'PrjTableName': 'pm_project', // Servicenow Project Table
         'TasksTableName': 'pm_project_task', // Servicenow Tasks Table
@@ -32,7 +32,7 @@ angular.module('starter.services', [])
             //get functions
             getProjects: function() {
                 var url = 'api/now/table/' + snCred.PrjTableName;
-                var token = "Bearer "+TokenService.getToken();
+                var token = "Bearer " + TokenService.getToken();
                 var defer = $q.defer()
                     //$http.defaults.headers.common.Authorization = "Bearer" + TokenService.getToken();
                 $http({
@@ -90,49 +90,44 @@ angular.module('starter.services', [])
             },
             getApprovals: function() {},
             getTimeCards: function() {},
-            getPendingTimeCardForCurrentDate: function(){
-                var url = 'api/now/table'+snCred.TimecardTable;
-                var token = "Bearer " + TokenService.getToken();
-                var defer = $q.defer;
-                $http({
-                    method: 'GET',
-                    url: url,
-                    headers:{
-                        'Authorization':token
-                    }
-                })
-                .success(function(data){
-                    console.log(data.result);
-                    defer.resolve(data.result);
-                })
-                .error(function(error){
-                    defer.reject(error);
-                })
-                return defer.promise;
-            }
-            //set functions 
+            getPendingTimeCardForCurrentDate: function() {
+                    var url = 'api/now/table' + snCred.TimecardTable;
+                    var token = "Bearer " + TokenService.getToken();
+                    var defer = $q.defer;
+                    $http({
+                            method: 'GET',
+                            url: url,
+                            headers: {
+                                'Authorization': token
+                            }
+                        })
+                        .success(function(data) {
+                            console.log(data.result);
+                            defer.resolve(data.result);
+                        })
+                        .error(function(error) {
+                            defer.reject(error);
+                        })
+                    return defer.promise;
+                }
+                //set functions 
             setTimeCard: function() {},
             submitTimeCard: function() {},
             deleteTimeCard: function() {}
         }
     })
-    .factory('InstanceUrlService', function() {
-        var url = ""
-        var url_oauth = url + 'oauth_token.do';
-
-    })
     .factory('UserDetailsService', function() {
         var user = {
-           'username':'',
-           'email':'',
-           'sys_id'
+            'username': '',
+            'email': '',
+            'sys_id'
         };
     })
     .factory('LoginService', function($http, $timeout, snCred) {
         var tokenUrl = "";
     })
     // localstorage lib from Ionic framework
-    .factory('DB', function($q,Loki) {
+    .factory('DB', function($q, Loki) {
 
 
     });
