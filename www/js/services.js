e3d8b916d3f2ccc117d78c9b19a8ee72e6b733d5
mrmaninew <manikanta@live.in>
2016-03-08 12:57:30 +0530

    made few changes in custom nav bards and stripped bars

--- www/js/services.js
+++ www/js/services.js
@@ -5,8 +5,8 @@ angular.module('starter.services', [])
         'Client_secret': '1yihwfk2xbl686v45s8a',
         'grant_type': ['password', 'access'],
         //'PRODURL':'', // Volteo ServiceNow Production Instance URL
-        'PRODURL': 'https://volteollcdemo1.service-now.com', // Volteo Servicenow development Instance URL
-        //'PRODURL': '/api', // Temp empty URL for development environment and this will changed when deploying PROD
+        //'PRODURL': 'https://volteollcdemo1.service-now.com', // Volteo Servicenow development Instance URL
+        'PRODURL': '/api', // Temp empty URL for development environment and this will changed when deploying PROD
         'PrjTableName': 'pm_project', // Servicenow Project Table
         'TasksTableName': 'pm_project_task', // Servicenow Tasks Table
         'StoriesTableName': 'rm_story', // Servicenow Stories Table
