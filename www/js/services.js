925c22dd80d7fbd96b5a9655672c66b0f942bab0
mrmaninew <manikanta@live.in>
2016-03-29 17:05:25 +0530

    made few changes by adding production client id and secret

--- www/js/services.js
+++ www/js/services.js
@@ -1,11 +1,13 @@
 angular.module('starter.services', [])
     // Required global variables
     .constant('snCred', {
-        'Client_id': 'ac0dd3408c1031006907010c2cc6ef6d',
-        'Client_secret': '1yihwfk2xbl686v45s8a',
+        //'Client_id':'ac0dd3408c1031006907010c2cc6ef6d', // Production Client ID
+        //'Client_secret':'a3kw6fydcqb3jum30opg', // Production Client Secret
+        'Client_id': 'ac0dd3408c1031006907010c2cc6ef6d', // "Development"
+        'Client_secret': '1yihwfk2xbl686v45s8a',  // "Development"
         'grant_type': ['password', 'access'],
-        'PRODURL':'https://volteopsa.service-now.com', // Volteo ServiceNow Production Instance URL
-        //'PRODURL': 'https://volteollcdemo1.service-now.com', // Volteo Servicenow development Instance URL
+        'PRODURL':'https://volteopsa.service-now.com', // "Production" Volteo ServiceNow Production Instance URL
+        //'PRODURL': 'https://volteollcdemo1.service-now.com', // "Development" Volteo Servicenow development Instance URL
         //'PRODURL': '/api', // Temp empty URL for development environment and this will changed when deploying PROD
         'PrjTableName': 'pm_project', // Servicenow Project Table
         'TasksTableName': 'pm_project_task', // Servicenow Tasks Table
